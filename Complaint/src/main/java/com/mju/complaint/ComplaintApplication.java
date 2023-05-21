f342d283b64a6311fd9a22a6ae4274892be669f3
duekdms <mmp7afy8@naver.com>
2023-05-21 16:26:33 +0900

    Initial commit

---
+++ Complaint/src/main/java/com/mju/complaint/ComplaintApplication.java
@@ -0,0 +1,15 @@
+package com.mju.complaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableFeignClients
+public class ComplaintApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintApplication.class, args);
+	}
+}
